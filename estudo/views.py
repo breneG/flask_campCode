fc4058f6075111efa877a010a31e20d1e0601edb
breneG <brenergomes11@hotmail.com>
2025-05-25 20:34:44 -0300

    Atualização do projeto

--- estudo/views.py
+++ estudo/views.py
@@ -1,5 +1,8 @@
-from estudo import app
-from flask import render_template, url_for
+from estudo import app, db
+from flask import render_template, url_for, request, redirect
+
+from estudo.models import Contato
+from estudo.forms import ContatoForm
 
 @app.route('/')
 def homepage():
@@ -12,6 +15,54 @@ def homepage():
     }
     return render_template('index.html', context=context)
 
-@app.route('/contatos/')
-def newpage():
-    return 'outra view'
+# Formato Recomendado
+@app.route('/contato/', methods=['GET', 'POST'])
+def contato():
+    form = ContatoForm()
+    context = {}
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('homepage'))
+        
+    return render_template('contato.html', context=context, form=form)
+
+@app.route('/contato/lista/')
+def contatoLista():
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa', '')
+    
+    dados = Contato.query.order_by('nome')
+    if pesquisa != '':
+        dados = dados.filter_by(nome=pesquisa)
+    
+    context = {'dados': dados.all()}
+    
+    return render_template('contato_lista.html', context=context)
+
+# Formato não recomendado
+@app.route('/contato_old/', methods=['GET', 'POST'])
+def contato_old():
+    context = {}
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        print('GET:', pesquisa)
+        context.update({'pesquisa': pesquisa})
+        
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+        
+        contato = Contato(
+            nome=nome,
+            email=email,
+            assunto=assunto,
+            mensagem=mensagem
+        )
+        
+        db.session.add(contato)
+        db.session.commit()
+        
+    return render_template('contato_old.html', context=context)
+
